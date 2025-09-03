d1015a7182a2ca9ed337999a60b808233bcf10e4
Waldo <fernandez_waldo@hotmail.com>
2025-09-03 10:49:58 -0300

    mejoras en lista de notificaciones

---
+++ front/src/utils/formatDate.jsx
@@ -0,0 +1,11 @@
+// componente para "tiempo simpático"
+export function timeAgo(date) {
+  const now = new Date();
+  const then = new Date(date);
+  const diffInSeconds = Math.floor((now - then) / 1000);
+
+  if (diffInSeconds < 60) return "hace unos segundos";
+  if (diffInSeconds < 3600) return `hace ${Math.floor(diffInSeconds / 60)} minutos`;
+  if (diffInSeconds < 86400) return `hace ${Math.floor(diffInSeconds / 3600)} horas`;
+  return `hace ${Math.floor(diffInSeconds / 86400)} días`;
+}
